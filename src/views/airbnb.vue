2b851ab6d0ade81a555be819ca773fbc4c1d0655
1ethon0822 <1ethyjj9240@gmail.com>
2026-01-13 10:23:15 +0900

    airbnb

---
+++ src/views/airbnb.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(async() => {
+    axios.get('/airbnb.json')
+    .then(res => console,log(res.data));
+
+});
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
