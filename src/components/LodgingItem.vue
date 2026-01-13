00cd384df67e3d0121c2a79a36ddff7978bdd1fe
1ethon0822 <1ethyjj9240@gmail.com>
2026-01-13 18:28:41 +0900

    airbnb1

---
+++ src/components/LodgingItem.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+    picture: String,
+    alt: String,
+    title: String,
+    period: String,
+    price: String,
+    rating: Number,
+    badge: String
+});
+</script>
+
+<template>
+    <div class="item po-rel">
+        <img :src="props.picture" :alt="props.alt">
+        <div>{{ props.title }}</div>
+        <div>{{ props.period }}</div>
+        <div>{{ props.price }} 
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 8px; width: 8px; fill: currentcolor;"><path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path></svg>
+            {{ props.rating }}
+        </div>
+        <div v-if="props.badge" class="badge">{{ props.badge }}</div>
+    </div>
+</template>
+
+<style scoped>
+.po-rel { position: relative; }
+.po-rel .badge { position: absolute; top: 10px; left: 10px; 
+    background-color: #ffffffab; padding: 5px 8px; 
+    border-radius: 10%;
+}
+.item { width: 200px; }
+.item:not(:first-child) { margin-left: 10px; }
+.item img { border-radius: 20px; width: 180px; height: 180px; }
+svg { display: inline !important; }
+
+</style>
--- src/globalstate.js
+++ src/globalstate.js
@@ -1,1 +1,1 @@
-export const globalState = ['component','page98','page103', 'page108','page110','page114','page118','page123','page125', 'page126','page129','mission01','airbnb' ];
+export const globalState = ['component','page98','page103', 'page108','page110','page114','page118','page123','page125', 'page126','page129','mission01','Airbnb' ];
